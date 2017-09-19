07aa487f7d97b3a7c1f24c937a6f39bb57ffdeaa
GabrieleFortunato <gabrielefortunato78@libero.it>
2017-09-19 14:09:38 +0200

    Commit del 19 settembre 2017 ore 14,09

--- digits_number/src/digits_number.c
+++ digits_number/src/digits_number.c
@@ -7,36 +7,3 @@
 
 #include "digits_number.h"
 
-void srand_time(){
-	srand(time(NULL));
-}
-
-int generate_number(){
-	int number = ONE + rand()%MAX_NUMBER;
-	assert(number>ZERO);
-	return number;
-}
-
-void print_number(FILE* file, int number){
-	assert(file!=NULL);
-	fprintf(
-			file,
-			NEW_LINE PRINT_NUMBER,
-			number
-	);
-}
-
-int digits_number(int number){
-	assert(number>ZERO);
-	return (number<TEN)?ONE:ONE+digits_number(number/TEN);
-}
-
-void print_digits_number(FILE* file, int number){
-	assert(file!=NULL);
-	fprintf(
-			file,
-			NEW_LINE PRINT_DIGITS,
-			digits_number(number)
-	);
-}
-
